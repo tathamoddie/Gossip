c9a51fce673058f6f282e94e7c530c896ab42a53
Tatham Oddie <tatham@oddie.com.au>
2013-11-07 15:17:11 +1100

    Stub routes

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Web
@@ -13,10 +9,14 @@ namespace Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Default", "", new { controller = "Home", action = "Index" });
+            routes.MapRoute("Join", "{key}", new { controller = "Event", action = "Join" });
+            routes.MapRoute("Host", "{key}/host", new { controller = "Event", action = "Host" });
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Fallback",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Web/Controllers/EventController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class EventController : Controller
+    {
+        public ActionResult Join(string key)
+        {
+            return View();
+        }
+
+        public ActionResult Host(string key)
+        {
+            return View();
+        }
+    }
+}
